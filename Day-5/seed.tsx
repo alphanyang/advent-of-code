a396b5fcd3f2883f9e1719940c49144c940c3351
Alphan Yang <90988780+alphanyang@users.noreply.github.com>
2024-08-09 01:48:23 -0400

    refactor day 5 less abstraction

--- Day-5/seed.tsx
+++ Day-5/seed.tsx
@@ -1,63 +1,35 @@
 import * as fs from 'fs'
 
 
-const input = fs.readFileSync('input.txt', 'utf-8')
-const lines = input.split('\n')
+const [input, ...blocks] = fs.readFileSync('input.txt', 'utf-8').split('\n\n')
 
-let maps = new Map<number, number[][]>()
-/*
-    Map => {50 98 02}
-           {52 50 48}
+let seeds: number[] = input.split(':')[1].trim().split(' ').map(Number) as number[]
 
-        if source# is not mapped to a destination, they remain the same
-        find the lowest final destination
-*/
+console.log(seeds)
 
-let lineCount = 0
-let slowPointer = 0
-let fastPointer = 1
-let key = ''
-while(fastPointer < lines.length) {
-    if(lines[slowPointer].includes('seeds:')) {
-        let seeds = lines[slowPointer].split(':')
-        maps.set(lineCount, [seeds[1].trim().split(' ').map(Number)])
-        slowPointer++
-    }
-
-    if(lines[fastPointer].includes('map')) {
-        slowPointer = fastPointer
-        // key = lines[slowPointer].split(' ')[0]
-        lineCount++
-        maps.set(lineCount, [])
-    } else if(lines[fastPointer] !== '') {
-        let temp = fastPointer - slowPointer - 1
-        const mapValue = maps.get(lineCount);
-        if (mapValue) {
-          mapValue[temp] = lines[fastPointer].trim().split(' ').map(Number)
-        }
-    } else if(lines[fastPointer] == '') {
-        slowPointer = fastPointer
-        // key = ''
-    }
-    fastPointer++
-}
-
-
-slowPointer = 0; fastPointer = 1
-let currentSoruce = maps.get(slowPointer)?.[0] ?? [];
-while(fastPointer <= lineCount) {
-    let changed = new Array<number>(currentSoruce.length).fill(0)
-    maps.get(fastPointer)?.forEach( row => {
-        for(let i = 0; i < currentSoruce.length; i++) {
-            let num = currentSoruce[i]
-            if(num >= row[1] && num < (row[1]) + (row[2]) && changed[i] == 0) {
-                currentSoruce[i] = num + ((row[0]) - (row[1]))
-                changed[i]++
-            }
-        }
+blocks.forEach(block => {
+    let ranges: number[][] = []
+    block.split('\n').slice(1).forEach( (num) => {
+        ranges.push(num.split(' ').map(Number) as number[])
     })
-    fastPointer++
-}
+    let newArray: number[] = []
+    seeds.forEach( (seed) => {
+        let found: boolean = false;
+        ranges.forEach( (range) => {
+            let [a,b,c] = range
+            if(b <= seed && seed < b + c) {
+                newArray.push(seed - b + a)
+                found = true;
+            }
+        })
+        if(!found) {
+            newArray.push(seed)
+        }
+        
+    })
+    seeds = newArray
+})
 
-currentSoruce = currentSoruce.sort((a, b) => a - b)
-console.log(currentSoruce[0])
+seeds.sort((a,b) => a-b)
+console.log(seeds[0])
+
